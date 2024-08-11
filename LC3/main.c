7d3d5a66ed278af1aaf812bd84cbdbc19ecf4157
XXI <ngmount2021@gmail.com>
2024-08-11 10:37:03 -0400

    Added support functions

--- LC3/main.c
+++ LC3/main.c
@@ -80,21 +80,18 @@ enum {
 // Input Buffering 
 struct termios original_tio;
 
-void disable_input_buffering()
-{
+void disable_input_buffering(){
     tcgetattr(STDIN_FILENO, &original_tio);
     struct termios new_tio = original_tio;
     new_tio.c_lflag &= ~ICANON & ~ECHO;
     tcsetattr(STDIN_FILENO, TCSANOW, &new_tio);
 }
 
-void restore_input_buffering()
-{
+void restore_input_buffering(){
     tcsetattr(STDIN_FILENO, TCSANOW, &original_tio);
 }
 
-uint16_t check_key()
-{
+uint16_t check_key(){
     fd_set readfds;
     FD_ZERO(&readfds);
     FD_SET(STDIN_FILENO, &readfds);
@@ -109,6 +106,27 @@ void handle_interrupt(int signal){
     restore_input_buffering();
     printf("\n");
     exit(-2);
+}
+
+uint16_t sign_extend(uint16_t x, int bit_count){
+    if ((x >> (bit_count - 1)) & 1){
+        x |= (0xFFFF << bit_count);
+    }
+    return x;
+}
+
+uint16_t swap16(uint16_t x){
+    return (x << 8) | (x >> 8);
+}
+
+void update_flags(uint16_t r){
+    if (reg[r] == 0){
+        reg[R_COND] = FL_ZRO;
+    } else if (reg[r] >> 15){
+        reg[R_COND] = FL_NEG;
+    } else {
+        reg[R_COND] = FL_POS;
+    }
 }
 
 // [MAIN]
