b4af5c2b6d7d851a4dcd51176393d858c19bb76e
XXI <ngmount2021@gmail.com>
2024-08-11 10:39:34 -0400

    Added image reading functs

--- LC3/main.c
+++ LC3/main.c
@@ -129,6 +129,30 @@ void update_flags(uint16_t r){
     }
 }
 
+void read_image_file(FILE* file){
+    uint16_t origin;
+    fread(&origin, sizeof(origin), 1, file);
+    origin = swap16(origin);
+
+    uint16_t max_read = MEM_MAX - origin;
+    uint16_t* p = memory + origin;
+    size_t read = fread(p, sizeof(uint16_t), max_read, file);
+
+    // Swap to little endian
+    while (read-- > 0){
+        *p = swap16(*p);
+        ++p;
+    }
+}
+
+int read_image(const char* image_path){
+    FILE* file = fopen(image_path, "rb");
+    if (!file) {return 0;}
+    read_image_file(file);
+    fclose(file);
+    return 1;
+}
+
 // [MAIN]
 int main(int argc, const char* argv[]){
 
