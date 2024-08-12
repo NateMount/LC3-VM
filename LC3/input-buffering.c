33fe9568a42006383df33cdda459ae750ca2c87f
XXI <ngmount2021@gmail.com>
2024-08-12 16:18:30 -0400

    Fixed errors with stdin

--- LC3/input-buffering.c
+++ LC3/input-buffering.c
@@ -8,18 +8,18 @@
 struct termios original_tio;
 
 void disable_input_buffering(){
-    tcgetattr(STDIN_FILENO, &original_tio);
-    struct termios new_tio = original_tio;
-    new_tio.c_lflag &= ~ICANON & ~ECHO;
-    tcsetattr(STDIN_FILENO, TCSANOW, &new_tio);
+  tcgetattr(STDIN_FILENO, &original_tio);
+  struct termios new_tio = original_tio;
+  new_tio.c_lflag &= ~ICANON & ~ECHO;
+  tcsetattr(STDIN_FILENO, TCSANOW, &new_tio);
 }
 
-void restore_input_buffering(){
-    tcsetattr(STDIN_FILENO, TCSANOW, &original_tio);
+void restore_input_buffering() {
+  tcsetattr(STDIN_FILENO, TCSANOW, &original_tio);
 }
 
 void handle_interrupt(int signal){
-    restore_input_buffering();
-    printf("\n");
-    exit(-2);
+  restore_input_buffering();
+  printf("\n");
+  exit(-2);
 }
